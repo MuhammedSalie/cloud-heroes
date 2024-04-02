b3e1c850e18c08d85b65194af761185383947be8
MuhammedSalie <muhammed.salie@mmltd.co.za>
2024-04-02 23:53:06 +0200

    setup gh-pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,9 @@
+import logo from './images/insights.gif';
 import React from "react";
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
-    <img src="./images/Insights.gif" height="80px" align="center" alt="Logo"/>
+    <img src={logo} height="80px" align="center" alt="Logo"/>
       Cloud Insights Copyright 2024      
     <header align="center">
     v1.0.1
